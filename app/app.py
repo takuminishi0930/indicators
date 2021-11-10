ca70e7ebc5fd44a29bca0b1c7c80367025af0ef4
takuminishi0930 <takuminishi0930@icloud.com>
2021-11-11 05:39:53 +0900

    modified

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import pandas as pd
 import datetime
 #Flaskオブジェクトの生成
@@ -12,7 +12,21 @@ def home():
     record = df.fillna('').values.tolist()
     MAX = list(df.max())
     MIN = list(df.min())
-    return render_template('index.html', header=header, record=record,MAX=MAX,MIN=MIN)
+    return render_template('index.html',header=header, record=record,MAX=MAX,MIN=MIN,date=twoDaysAgo)
+
+@app.route("/indicators",methods=["POST"])
+def indicators():
+    date = request.form.get("d")
+    if request.form.get("b")=="":
+        month = date-datetime.timedelta(month=1)
+    elif request.form.get("n")=="":
+        month = date+datetime.timedelta(month=1)
+    df = pd.read_csv('app/static/csv/{}.csv'.format(month.strftime('%Y-%m'), encoding="utf-8"))
+    header = df.fillna('').columns.tolist()
+    record = df.fillna('').values.tolist()
+    MAX = list(df.max())
+    MIN = list(df.min())
+    return render_template('index.html',header=header, record=record,MAX=MAX,MIN=MIN,date=month)
 
 if __name__ == '__main__':
     app.run()
