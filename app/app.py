03371a4beed5b5ba4773e72354c048ee4eac207f
takuminishi0930 <takuminishi0930@icloud.com>
2021-11-11 03:52:17 +0900

    modified

--- app/app.py
+++ app/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    df = pd.read_csv('app/static/csv/indicators.csv', encoding="utf-8", na_filter=False)
-    header = df.columns.tolist()
-    record = df.values.tolist()
+    # df = pd.read_csv('app/static/csv/indicators.csv', encoding="utf-8", na_filter=False)
+    df = pd.read_csv('app/static/csv/indicators.csv', encoding="utf-8")
+    header = df.fillna('').columns.tolist()
+    record = df.fillna('').values.tolist()
     MAX = list(df.max())
     MIN = list(df.min())
     return render_template("index.html", header=header, record=record,MAX=MAX,MIN=MIN)
